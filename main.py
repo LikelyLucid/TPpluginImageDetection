9dd5ab22be9f648c6d6e8685e2d3fe4941232ac1
PurelyStupidity <micoolplays@gmail.com>
2022-01-27 17:24:25 +1300

    January 27, 2022 5:24 PM

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ def onShutdown(data):
 # Connect to Touch Portal and block (wait) until disconnected
 TPClient.connect()
 
-# add comments to the code above t oshow how the code works
+# add comments to the code above to show how the code works
 import mss
